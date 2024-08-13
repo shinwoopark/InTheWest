43304e88cfca75a9cb8f220480feb6e56c08474a
shinwoopark <shinwoo060310@gmail.com>
2024-08-14 00:48:36 +0900

    s

--- in the west/Assets/Scripts/Enemy/EnemyType/Boss1.cs
+++ in the west/Assets/Scripts/Enemy/EnemyType/Boss1.cs
@@ -13,10 +13,20 @@ enum State
 public class Boss1 : MonoBehaviour
 {
     private EnemySystem _enemySystem;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+    private PlayerSystem _playerSystem;
+    private GameObject _player_gb;
 
     private void Awake()
     {
         _enemySystem = GetComponent<EnemySystem>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+
+        _player_gb = GameObject.Find("Player");
+        _playerSystem = _player_gb.GetComponent<PlayerSystem>();
     }
 
     void Start()
@@ -29,5 +39,13 @@ public class Boss1 : MonoBehaviour
         
     }
 
+    private void ChoosePattern()
+    {
 
+    }
+
+    private void Pattern1()
+    {
+
+    }
 }
